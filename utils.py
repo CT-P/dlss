5533980d82e10090faf1470673f9c1e0bc32ffab
CT-P <trindade.catarina@hotmail.com>
2023-02-28 17:59:28 +0100

    adding files

---
+++ utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize(filename):
+
+    data = np.loadtxt(fname=filename, delimiter=',')
+
+    fig = plt.figure(figsize=(10.0, 3.0))
+
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
+
+    axes1.set_ylabel('average')
+    axes1.plot(np.mean(data, axis=0))
+
+    axes2.set_ylabel('max')
+    axes2.plot(np.max(data, axis=0))
+
+    axes3.set_ylabel('min')
+    axes3.plot(np.min(data, axis=0))
+
+    fig.tight_layout()
+    plt.show()
